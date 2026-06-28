24da14021afd7ba323529cae693e3f7b1d20e852
Rishi M G <rishikrishnan87@gmail.com>
2026-06-28 14:13:04 -0400

    chore(search): added tsvector column to articles table & modified articleRepository

--- backend/src/main/java/com/articlemanager/backend/Repository/ArticleRepository.java
+++ backend/src/main/java/com/articlemanager/backend/Repository/ArticleRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.articlemanager.backend.entity.Articles;
 
@@ -11,4 +13,12 @@ public interface ArticleRepository extends JpaRepository<Articles, Long> {
     Optional<Articles> findBySlug(String slug);
 
     List<Articles> findBySlugStartingWith(String slugPrefix);
+
+    @Query(value = """
+            SELECT * FROM articles
+            WHERE status = 'PUBLISHED'
+            AND search_vector @@ websearch_to_tsquery('english',:query)
+            ORDER BY ts_rank(search_vector, websearch_to_tsquery('english', :query)) DESC
+            """, nativeQuery = true)
+    List<Articles> searchPublishedArticles(@Param("query") String query);
 }
